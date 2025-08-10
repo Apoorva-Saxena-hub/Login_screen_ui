58180aac2b6086ff3517276595fe57c980944e01
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-08-10 13:48:02 +0530

    changes in login_field

--- lib/widgets/login_field.dart
+++ lib/widgets/login_field.dart
@@ -5,6 +5,6 @@ class LoginField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextFormField(decoration: InputDecoration());
   }
 }
